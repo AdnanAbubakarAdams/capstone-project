c31ec6bc7446e73300fb680eaa81f9e140845647
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-09 21:24:40 -0400

    updates on the query for creating request which was returning an error

--- back-end/queries/requests.js
+++ back-end/queries/requests.js
@@ -32,7 +32,7 @@ const makeRequest = async (request) => {
   try {
     console.log("QUERY : Adding request to database");
     request = await db.one(
-      "INSERT INTO requests (elder_id, description, req_date, location, time, title, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+      "INSERT INTO requests (elder_id, elder_img, description, req_date, location, time, title, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
       [
         request.elder_id,
         request.elder_img,
